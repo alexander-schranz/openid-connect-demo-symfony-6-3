69ed5150d8ff78743d354c9b30f5580ff52a0769
Alexander Schranz <alexander@sulu.io>
2023-07-04 15:40:56 +0200

    Add a basic login over doctrine ORM (#3)

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
