49d3a9abf5f3eed63e03f420f27df75d78dad98a
Alexander Schranz <alexander@sulu.io>
2023-07-04 15:55:44 +0200

    Add custom orm user provider

---
+++ src/Security/UserProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {}
+
+    /**
+     * Symfony calls this method if you use features like switch_user
+     * or remember_me. If you're not using these features, you do not
+     * need to implement this method.
+     *
+     * @throws UserNotFoundException if the user is not found
+     */
+    public function loadUserByIdentifier(string $identifier): UserInterface
+    {
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $identifier]);
+
+        if (!$user) {
+            throw new UserNotFoundException(sprintf('User "%s" not found.', $identifier));
+        }
+
+        return $user;
+    }
+
+    /**
+     * Refreshes the user after being reloaded from the session.
+     *
+     * When a user is logged in, at the beginning of each request, the
+     * User object is loaded from the session and then this method is
+     * called. Your job is to make sure the user's data is still fresh by,
+     * for example, re-querying for fresh User data.
+     *
+     * If your firewall is "stateless: true" (for a pure API), this
+     * method is not called.
+     */
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Invalid user class "%s".', get_class($user)));
+        }
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+
+        if (!$user) {
+            throw new UserNotFoundException(sprintf('User "%s" not found.', $user->getEmail()));
+        }
+
+        return $user;
+    }
+
+    /**
+     * Tells Symfony to use this provider for this User class.
+     */
+    public function supportsClass(string $class): bool
+    {
+        return User::class === $class || is_subclass_of($class, User::class);
+    }
+}
